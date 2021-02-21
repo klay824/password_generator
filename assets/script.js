0d7cb5f28ef3605ae0c0e4e4a4cc625ec8ac364a
klay824 <kchadwell0226@gmail.com>
2021-02-20 20:23:04 -0600

    noticed the password was generating with a lot of commas, changed how I had my character arrays, also added some line breaks in the prompts

--- assets/script.js
+++ assets/script.js
@@ -7,55 +7,55 @@ generateBtn.addEventListener("click", writePassword);
 
 // Prompts and loops
 function generatePassword() {
-  var pwLength = (prompt("How many characerters would you like your password to be?"));
+  var pwLength = (prompt("How many characters long would you like your password to be?"));
 
-  while(pwLength <= 7 || pwLength >= 128) {
-    alert("Your password must be at least 8 characters long, but less than 128.");
+  while(pwLength < 8 || pwLength > 128) {
+    alert("Your password must be at least 8 characters long and no more than 128.");
     var pwLength = (prompt("How many characters would you like your password to be?"));
     }
 
     alert("Your password will be " + pwLength + " characters long.");
   
-  var pwUpperCase = (confirm("Would you like your password to contain uppercase letters? OK for yes; Cancel for No."));
+  var upperCaseConfirm = (confirm("Would you like your password to contain uppercase letters? \n\nClick OK for Yes; Cancel for No."));
     
-  var pwLowerCase = (confirm("Would you like your password to contain lowercase letters? OK for yes; Cancel for No."));
+  var lowerCaseConfirm = (confirm("Would you like your password to contain lowercase letters? \n\nClick OK for Yes; Cancel for No."));
 
-  var pwSpecial = (confirm("Would you like your password to contain special characters? OK for yes; Cancel for No."));
+  var specialConfirm = (confirm("Would you like your password to contain special characters? \n\nClick OK for Yes; Cancel for No."));
 
-  var pwNumber = (confirm("Would you like your password to contain numbers? OK for yes; Cancel for No."));
+  var numberConfirm = (confirm("Would you like your password to contain numbers? \n\nClick OK for Yes; Cancel for No."));
 
-    if(pwUpperCase === false && pwLowerCase === false && pwSpecial === false && pwNumber === false) {
-      alert("You must select at least one of the following: \r\n Uppercase, Lowercase, Special Character, or Number.");
-      var pwUpperCase = (confirm("Would you like your password to contain uppercase letters? OK for yes; Cancel for No."));
+    if(upperCaseConfirm === false && lowerCaseConfirm === false && specialConfirm === false && numberConfirm === false) {
+      alert("You must select at least one of the following: \nUppercase, Lowercase, Special Character, or Number.");
+      var upperCaseConfirm = (confirm("Would you like your password to contain uppercase letters?\n\nClick OK for yes; Cancel for No."));
       
-      var pwLowerCase = (confirm("Would you like your password to contain lowercase letters? OK for yes; Cancel for No."));
+      var lowerCaseConfirm = (confirm("Would you like your password to contain lowercase letters?\n\nClick OK for yes; Cancel for No."));
 
-      var pwSpecial = (confirm("Would you like your password to contain special characters? OK for yes; Cancel for No."));
+      var specialConfirm = (confirm("Would you like your password to contain special characters?\n\nClick OK for yes; Cancel for No."));
 
-      var pwNumber = (confirm("Would you like your password to contain numbers? OK for yes; Cancel for No."));
+      var numberConfirm = (confirm("Would you like your password to contain numbers?\n\nClick OK for yes; Cancel for No."));
     }
   
-    var pwCharacters = []; // empty array for the concatenated arrays below to be passed into
+  var pwCharacters = []; // empty array for the concatenated arrays below to be passed into
   
-  if(pwUpperCase) {
-    pwCharacters = pwCharacters.concat(upperCase);
+  if(upperCaseConfirm) {
+    pwCharacters = pwCharacters + upperCase;
   }  
 
-  if(pwLowerCase) {
-    pwCharacters = pwCharacters.concat(lowerCase);
+  if(lowerCaseConfirm) {
+    pwCharacters = pwCharacters + lowerCase;
   }
 
-  if(pwSpecial) {
-    pwCharacters = pwCharacters.concat(special);
+  if(specialConfirm) {
+    pwCharacters = pwCharacters + special;
   }
 
-  if(pwNumber) {
-    pwCharacters = pwCharacters.concat(number);
+  if(numberConfirm) {
+    pwCharacters = pwCharacters + number;
   }
 
   console.log(pwCharacters);
 
-    var completePassword = ""; // empty string for the results of the below randomizer to be passed through to
+  var completePassword = ""; // empty string for the results of the below randomizer to be passed through to
   
     for (var i = 0; i < pwLength; i++) {
       completePassword = completePassword + pwCharacters[Math.floor(Math.random() * pwCharacters.length)];
@@ -64,10 +64,10 @@ function generatePassword() {
 }
 
 // Arrays
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "~"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var special = ["~`!@#$%^&*()_-{}[]|,.<>/?';:"];
+var number = ["0123456789"];
 
 // Write password to the #password input
 function writePassword() {
